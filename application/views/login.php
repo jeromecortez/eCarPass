cafe51ac0de0bdbce939667ac73eea3e953dbe71
Miggy Madet <miggymadet22@gmail.com>
2018-01-20 18:12:24 +0900

    Removed space

--- application/views/login.php
+++ application/views/login.php
@@ -36,7 +36,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>eCarPass - a Parking Area Access</title>
 
         <!-- Bootstrap Core CSS -->
